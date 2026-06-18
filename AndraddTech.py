8248521845e0565998b6149fd0e781147b68bb59
Guilherme Andrade <guilhermeandrade5979@gmail.com>
2026-06-17 21:09:10 -0300

    Adicinei funcionalidade de voltar BR ao clicar 'backspace'. Melhorei a visibilidade do usuario.

--- AndraddTech.py
+++ AndraddTech.py
@@ -4,25 +4,27 @@ import tkinter as tk # É a ferramneta q cria janela, e eli eu digo q vou resumi
 import pyperclip # essa ferramenta le oq esta no "Ctrl + C" do ususario.
 import time
 LARGURA = 108
-
+print("\033[97m")
 print("╔" + "═" * (LARGURA - 2) + "╗")
 print("║" + "ANDRADE TECH".center(LARGURA - 2) + "║")
 print("║" + "Leitor Inteligente de BRs".center(LARGURA - 2) + "║")
-print("║" + "           Desenvolvido por Guilherme De Andrade".center(LARGURA - 13) + "Version 1.3║")
+print("║" + "           Desenvolvido por Guilherme De Andrade".center(LARGURA - 13) + "Version 1.4║")
 print("╚" + "═" * (LARGURA - 2) + "╝")
+print("\033[0m")
 print()
 
 print()
 print()
 
-print("Opaaa, Tudo Bem??")
-print("Meu Analista Lindo, Vamos começar!!")
-print("LEMBRETE: A janela do QRcode pode estar atrás desta janela após iniciar. Fique Atendo ao iniciar!!")
-print('ATENÇÂO: \033[31mNão precisa colar "Ctrl + V"! Apenas copie as BRs onde quer q for, e de ENTER no programa!!\033[0m')
+print("\033[97mOpaaa, Tudo Bem??\033[97m")
+print("\033[97mMeu Analista Lindo, Vamos começar!!\033[97m")
+print("\033[33mLEMBRETE\033[0m: \033[35mA janela do QRcode pode estar atrás desta janela após iniciar. Fique Atento ao iniciar!!\033[35m")
+print('\033[33mATENÇÂO\033[0m: \033[34mNão precisa colar "Ctrl + V"! Apenas copie as BRs onde quer q for, e de ENTER no programa!!\033[34m')
+print('\033[33mMODO DE USO\033[0m: \033[32mESPAÇO = Próxima BR\033[0m | \033[31mBACKSPACE = BR anterior\033[0m')
 print()
 
 while True:
-    input('Após ter copiado Clique \033[32mENTER\033[0m: ') # Aqui eu falo para o Analista ou operador do programa por as BRs.
+    input('\033[97mApós ter copiado Clique\033[97m \033[32mENTER\033[0m\033[97m:\033[97m ') # Aqui eu falo para o Analista ou operador do programa por as BRs.
     brs_acumuladas = pyperclip.paste() # Assim q ele cliclar isso pega oq esta no "Ctrl + C" do ususario.
     brs_acumuladas = brs_acumuladas.replace("\n", " ").replace("\t", " ").replace("\r", " ").strip()
     brs_acumuladas_verificar = brs_acumuladas.replace(" ", "").isalnum()
@@ -30,7 +32,7 @@ while True:
     if brs_acumuladas_verificar == True:
         break
     print('ERRO: Analista!! Você copiou errado, tem algum ".", "," ou caracter especial noque você copiou. Copie novamente e de ENTER!')
-
+    print()
 print()
 print()
 
@@ -50,13 +52,13 @@ def mostrar_br(): # Essa função atualiza a tela
     img = qrcode.make(br) # Manda fazer um QRcode com o texto na variavel "br".
     img_tk = ImageTk.PhotoImage(img) # O "tkinter" nao sabe mostrar este tipo de imagem "img" Então aqui to covertendo ela para uma forma q ele entenda e consiga mostrar.
 
-    texto.config(text=f"BR: {br}\n{indice + 1} de {quantidade_de_br}\n{((indice + 1) / quantidade_de_br) * 100:.0f}% Cocluido\nAperte ESPAÇO para ir para a próxima")# Coloca um texto na tela, é tipo um "print".
+    texto.config(text=f"BR: {br}\n{indice + 1} de {quantidade_de_br}\n{((indice + 1) / quantidade_de_br) * 100:.0f}% Concluido")# Coloca um texto na tela, é tipo um "print".
 
     qr.config(image=img_tk) # Atualiza a imagem mostrada no Label do QR Code.
 
 
 
-def proxima_br(espaco):  # Essa função é chamada quando apertar espaço
+def proxima_br(space):  # Essa função é chamada quando apertar espaço
 
     global indice # Pega a variavel "indice" fora da função e coloca dentro.
 
@@ -65,8 +67,21 @@ def proxima_br(espaco):  # Essa função é chamada quando apertar espaço
     # Se acabou a lista fecha o programa
     if indice >= len(lista_brs):
         janela.destroy()
-        return # Para Função se oq o "if" diz acontecer.
+        return 
 
+    mostrar_br()
+
+
+def voltar_br(backspace):  # Essa função é chamada quando apertar espaço
+
+    global indice # Pega a variavel "indice" fora da função e coloca dentro.
+
+    indice -= 1
+
+    
+    if indice < 0:
+        indice = 0
+        return 
     mostrar_br()
 
 
@@ -75,15 +90,20 @@ janela = tk.Tk()# Cria uma nova janela q vai mostrar o QRcode/ tipo abrir um pro
 janela.title("Leitor de BR") # Titulo da janela.
 
 # Cria o Label do texto
-texto = tk.Label(janela, font=("Arial", 12))
+texto = tk.Label(janela, font=("Arial", 14))
 texto.pack() # Coloca o texto do "Label" dentro da janela.
 
 qr = tk.Label(janela) # Cria o "Label"" da imagem vazio.
 qr.pack()# Coloca o "Label" imagem na tela.
 
+instrucoes = tk.Label(janela, font=("Arial", 14), text="ESPAÇO → Próxima | BACKSPACE → Anterior")
+instrucoes.pack()
+
 mostrar_br() # Executa a funçao, mostrando a primeira BR.
 
 janela.bind("<space>", proxima_br) # Quando apertar espaço chama a função proxima_br
+
+janela.bind("<BackSpace>", voltar_br)
 
 janela.mainloop()# Mantém a janela aberta
 
@@ -93,8 +113,10 @@ tempo = (inicio - fim) * -1
 tempo_minutos = tempo // 60
 tempo_segundos = tempo % 60
 
+print()
+
 while True:
-    print(f"\rAcabou!!!!! Você demorou {tempo_minutos:.0f} minutos e {tempo_segundos:.0f} segundos para percorrer todos os BRs!!")
+    print(f"\033[33mAcabou!!!!!\033[0m Você demorou \033[32m{tempo_minutos:.0f}\033[0m minutos e \033[32m{tempo_segundos:.0f}\033[0m segundos para percorrer todos os \033[32m{len(lista_brs)}\033[0m BRs!!")
     if True:
       input("Clique 'ENTER' para fechar o programa: ")
     break
